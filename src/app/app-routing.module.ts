1e43c5e8b6294acb3f9351bd04fe3adba592f841
TCHAKAL <ahmed.tchakal@gmail.com>
2022-08-29 14:13:55 +0100

    Ajouter le Landing page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import {NgModule} from "@angular/core";
 import {RouterModule, Routes} from "@angular/router";
 import {SnapFaceListComponent} from "./snap-face-list/snap-face-list.component";
+import {LandingPageComponent} from "./landing-page/landing-page.component";
 
 const routes: Routes = [
   {
     path: 'facesnaps',
     component: SnapFaceListComponent
+  },
+  {
+    path: '',
+    component: LandingPageComponent
   }
 ];
 

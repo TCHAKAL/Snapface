f6c00e25dbdb5c6f9147cdedaf30b5580c3e0391
TCHAKAL <ahmed.tchakal@gmail.com>
2022-08-30 09:37:17 +0100

    Ajout d'un observable interval

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {interval, Observable} from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -7,9 +8,10 @@ import {Component, OnInit} from '@angular/core';
 })
 export class AppComponent implements OnInit {
 
-
   ngOnInit() {
-
+    const interval$ = interval(1000);
+    interval$.subscribe(value => console.log("1ere instance "+value));
+    setTimeout(()=> interval$.subscribe(value => console.log("2eme instance "+value)),3000);
   }
 }
 

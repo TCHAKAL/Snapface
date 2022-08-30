fe9482198a772a4d3dee4b6b726b404b5fcd4d69
TCHAKAL <ahmed.tchakal@gmail.com>
2022-08-30 10:36:33 +0100

    Manipuler les émissions avec les opérateurs bas niveau pipe() filter() map()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {interval, Observable} from "rxjs";
+import {filter, interval, map, Observable} from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -7,10 +7,15 @@ import {interval, Observable} from "rxjs";
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  interval$!: Observable<number>;
+  interval$!: Observable<string>;
 
   ngOnInit() {
-    this.interval$ = interval(1000);
+    this.interval$ = interval(1000).pipe(
+      filter(value => value % 3 === 0),
+      map(value => value % 2 === 0 ?
+        'Je suis ' + value + ' et je suis pair' :
+        'Je suis ' + value + ' et je suis impair')
+    );
   }
 }
 
